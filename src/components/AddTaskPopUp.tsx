e47f62bfbe0c1e0dda76c7ca43a1693af0d130b3
Abhijith-TG <abhijithtg62@gmail.com>
2025-04-15 22:13:19 +0530

    style: update AddTaskPopUp component layout and improve input styling

--- src/components/AddTaskPopUp.tsx
+++ src/components/AddTaskPopUp.tsx
@@ -1,12 +1,13 @@
 
 export default function AddTaskPopUp() {
   return (
-    <div className="rounded p-3 bg-amber-600 " >
-        <div>
-          <span></span><input type="text" placeholder="Create a new task" />
+    <div className="rounded p-3 bg-white shadow " >
+        <div className="bg-gray-100 w-full flex items-center  px-2 mb-2">
+          <span className="w-5 h-5 rounded  border border-blue-500"></span><input className="px-4 py-2 outline-none w-[300px] md:w-[400px]" type="text" placeholder="Create a new task" />
         </div>
-        <div>
-          <div>
+        <div className=" w-full flex items-center gap-1 px-2 border rounded border-gray-200">
+          <div className="flex items-center  " >
+          <span className="w-5 h-5 rounded  border border-blue-500"></span><input className="px-4 py-2 outline-none w-[300px] md:w-[400px]" type="text" placeholder="No Lists Selected..." />
 
           </div>
           <div>
